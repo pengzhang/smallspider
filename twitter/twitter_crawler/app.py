cf85dd12f953eb78cf8691fdc1c9a041118703e2
ZhangPeng <zp8360@qq.com>
2022-09-06 08:33:47 +0000

    twitter_crawler 添加HttpAPI支持

---
+++ twitter/twitter_crawler/app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from twitter_crawler.api import get_api
+import json
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello, World!"
+
+
+@app.route("/twitter")
+def twitter():
+    api = get_api({})
+    result = api.get_user_timeline(screen_name='pantwtwtw', count=1)
+    # result = api.get_tweet_comments(post_id="1467435680968105990", count=10000)
+    return json.dumps(result, ensure_ascii=False, indent=4)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=80)
