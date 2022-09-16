f36e49e705229e635b411ce24bf91070d39fe4cf
ZhangPeng <zp8360@qq.com>
2022-09-16 09:09:57 +0000

    调整代码目录结构

--- facebook/facebook_crawler/test.py
+++ facebook/facebook_crawler/test.py
@@ -1,5 +1,5 @@
 from facebook_scraper import get_posts
 
-for post in get_posts('zuck', cookies="cookies.txt", pages=10):
+for post in get_posts('zuck', cookies="cookies.txt", pages=2):
     print(post)
     #print(post['text'][:50])
